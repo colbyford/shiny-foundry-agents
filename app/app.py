b12ee02545da26adbda066964a450750b3df68f2
Colby Ford <colbytylerford@gmail.com>
2026-02-14 12:14:13 -0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,39 +1,26 @@
-# ------------------------------------------------------------------------------------
-# A basic Shiny Chat example powered by OpenAI running on Azure.
-# To run it, you'll need a deployed model and API key from Microsoft Foundry.
-# ------------------------------------------------------------------------------------
+## ------------------------------------------------------------------
+## A basic Shiny Chat example powered by a Microsoft Foundry Agent.
+## ------------------------------------------------------------------
+
 import os
-
-# from dotenv import load_dotenv
 from app_utils import load_dotenv
-from openai import AzureOpenAI, OpenAI
-
+from openai import OpenAI
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider 
 from shiny.express import ui
 
-## Load environment variables from .env file
+## Load environment variables from .env file (for local testing)
 load_dotenv()
 
-## Check that .env variables are set
-print("FOUNDRY_API_KEY:", os.getenv("FOUNDRY_API_KEY") is not None)
-print("FOUNDRY_ENDPOINT:", os.getenv("FOUNDRY_ENDPOINT") is not None)
-print("FOUNDRY_DEPLOYMENT_NAME:", os.getenv("FOUNDRY_DEPLOYMENT_NAME") is not None)
-
-llm = AzureOpenAI(
-    api_key=os.getenv("FOUNDRY_API_KEY"),
-    api_version="2024-02-01",
-    azure_endpoint=os.getenv("FOUNDRY_ENDPOINT"),
-)
-
-# llm = OpenAI(
-#     base_url=os.getenv("FOUNDRY_ENDPOINT"),
-#     api_key=os.getenv("FOUNDRY_API_KEY")
-# )
-
-deployment_name = os.getenv("FOUNDRY_DEPLOYMENT_NAME")
+## Create connection to Foundry Agent using AsyncOpenAI client and Azure credentials
+client = OpenAI(
+    api_key=get_bearer_token_provider(DefaultAzureCredential(), "https://ai.azure.com/.default"),
+    base_url=os.getenv("FOUNDRY_ENDPOINT"),
+    default_query = {"api-version": "2025-11-15-preview"}
+    )
 
 ## Set some Shiny page options
 ui.page_opts(
-    title="Welcome to Foundry Agent Chat",
+    title="Microsoft Foundry - Real Estate Agent 🏡",
     fillable=True,
     fillable_mobile=True,
 )
@@ -49,17 +36,21 @@ chat = ui.Chat(
 ## Display the chat
 chat.ui()
 
-
 ## Define a callback to run when the user submits a message
 @chat.on_user_submit
 async def _():
     ## Get messages currently in the chat
     messages = chat.messages(format="openai")
     ## Create a response message stream
-    response = llm.chat.completions.create(
-        model=deployment_name,
-        messages=messages,
-        stream=True,
-    )
-    ## Append the response stream into the chat
-    await chat.append_message_stream(response)
+    response = client.responses.create( 
+        input=messages,
+        stream=True
+        ) 
+    ## Stream the response tokens into the chat as they arrive
+    async def stream_tokens():
+        for chunk in response:
+            if chunk.type == "response.output_text.delta":
+                yield chunk.delta
+
+    await chat.append_message_stream(stream_tokens())
+
